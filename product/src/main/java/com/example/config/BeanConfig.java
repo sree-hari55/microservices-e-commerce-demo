066e695edb600725c6d9abc699b776d36475ba33
Srihari G <srihari.g@sritindia.com>
2021-01-27 15:52:23 +0530

    adding actuators

--- product/src/main/java/com/example/config/BeanConfig.java
+++ product/src/main/java/com/example/config/BeanConfig.java
@@ -1,6 +1,8 @@
 package com.example.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,12 @@ public class BeanConfig {
 	@Bean
 	public ModelMapper modalMapper() {
 		return new ModelMapper();
+	}
+	
+	
+	// to enable Http trace endpoint in Actuator
+	@Bean 
+	HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 }
